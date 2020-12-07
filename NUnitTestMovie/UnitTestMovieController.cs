3c94d09c54bcf8f91df186a032753c3fe39f83fd
Matthias Colin <matthias.colin@gmail.com>
2020-12-07 07:43:00 +0100

    end training: last tests + tableau

---
+++ NUnitTestMovie/UnitTestMovieController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+using MovieRestApi.Services;
+using MovieRestApi.Controllers;
+using MovieRestApi.Models;
+using Moq;
+
+// article: https://exceptionnotfound.net/using-moq-to-create-fluent-test-classes-in-asp-net-core/
+
+namespace UnitTestMovie
+{
+    public class UnitTestMovieController
+    {
+        [Fact]
+        public async Task GetMovieFoundTest() {
+            var id = 1L;
+            // create mock
+            var mockMovieService = new Mock<IMovieService>();
+            // branchement
+            var moviesController = new MoviesController(mockMovieService.Object); 
+            // arrange : future réponse interne
+            mockMovieService.Setup(s => s.GetMovie(id))
+                    .Returns(Task<Movie>.FromResult(new Movie { Id = id, Title = "Joker", Year = 2019 }));
+            // when 
+            var actionResultMovie = await moviesController.GetMovie(id);
+            // check
+            var movie = actionResultMovie.Value;
+            Assert.NotNull(movie);
+            Assert.Equal(id, movie.Id);
+            // verify : appel vers le mock
+            // mockMovieService.Verify(s => s.GetMovie(id), Times.Once());
+            mockMovieService.Verify(s => s.GetMovie(It.IsAny<long>()), Times.Once());
+        }
+
+        [Fact]
+        public async Task GetMovieNotFoundTest() {
+            // exercice: id => no movie stored
+            // create mock
+            var mockMovieService = new Mock<IMovieService>();
+            // branchement
+            var moviesController = new MoviesController(mockMovieService.Object);
+            // arrange : future réponse interne
+            mockMovieService.Setup(s => s.GetMovie(It.IsAny<long>()))
+                    .Returns(Task<Movie>.FromResult<Movie>(null));
+            // when 
+            var actionResultMovie = await moviesController.GetMovie(1);
+            // check
+            var movie = actionResultMovie.Value;
+            var result = actionResultMovie.Result;
+            Assert.Null(movie);
+            Assert.NotNull(result);
+            Assert.IsType<NotFoundResult>(result);
+
+            // mockMovieService.Verify(s => s.GetMovie(id), Times.Once());
+            mockMovieService.Verify(s => s.GetMovie(It.IsAny<long>()), Times.Once());
+        }
+
+    }
+}
---
+++ NUnitTestMovie/UnitTestMovieService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+using MovieRestApi.Models;
+using MovieRestApi.Services;
+using MovieRestApi.Services.Impl;
+using System.Threading.Tasks;
+
+namespace UnitTestMovie
+{
+    public class UnitTestMovieService
+    {
+        [Fact]
+        public void GetMovieSuccesTest()
+        {
+            // Arrange
+            var mockMovieRepo = new Mock<MovieContext>();
+            var movieService = new MovieService(mockMovieRepo.Object);
+            //mockMovieRepo.Setup(c =>
+            //        c.Movies.FindAsync(1)) //It.IsAny<long>)
+            //        .Returns(Task.FromResult(new Movie())));
+            // when
+            //var movie = await movieService.GetMovie(1);
+            // then 
+            //Assert.NotNull(movie);
+            // verify
+            mockMovieRepo.Verify(c => c.Movies.FindAsync(1), Times.Once());
+        }
+            
+    }
+}
